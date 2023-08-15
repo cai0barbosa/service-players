dbf932813ad357af6c130cf620fd7b0b67f3c80b
Caio Augusto <caio@attingedigital.com.br>
2023-08-15 16:21:12 -0300

    init: microservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { PlayersModule } from './players/players.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    PlayersModule,
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
